2a86758199c6eec891a3c23433f5623219c2fe67
davide <davide.baiocchi97@gmail.com>
2021-05-26 14:42:13 +0200

    end

---
+++ Js/myscript.js
@@ -0,0 +1,14 @@
+// Nome 
+var userName = prompt("Hey, come ti chiami?");
+console.log(userName);
+
+// Cognome 
+var surname = prompt("Il tuo cognome?");
+console.log(surname);
+
+// Colore preferito 
+var color = prompt("Colore preferito?");
+console.log(color);
+
+// Risultato 
+document.getElementById("password").innerHTML = userName + surname + color + "21";
